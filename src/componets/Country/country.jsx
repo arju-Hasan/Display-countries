781d1b52f7e150de2167d1cc94502864bb6a435e
Arju-Hasan <rajuahmeed698@gmail.com>
2025-09-22 13:20:07 +0600

    add a  another css file and onclick button

--- src/componets/Country/country.jsx
+++ src/componets/Country/country.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './country.css'
+
+
+
+
 const Country = ({ country }) => {
+  const [visited, setvisited] = useState(false);
+
+const handleNotVisited = () => {
+  // console.log("button clicked");
+  setvisited(true);
+}
+
     return (
-      <div className="border-2 m-4 p-4 rounded-lg">
+      <div className="country">
         <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
         <h3>Name: {country.name.common}</h3>
         <h4>Capital: {country.capital.capital}</h4>
         <h4>Region: {country.region.region}</h4>
         <h4>Population: {country.population.population}</h4>
+        <p>Area: {country.area.area}{country.area.area > 1000000 ? " (Large)" : " (Small)"}</p>
+        <button onClick={handleNotVisited} className='not-visited-btn'>
+          { visited ? "Visited" : "Not Visited" }
+          </button>
       </div>
     );
 };
--- src/componets/countries/countries.jsx
+++ src/componets/countries/countries.jsx
@@ -1,20 +1,23 @@
-import React, { use } from 'react'
-import Country from '../Country/country';
+import React, { use } from "react";
+import Country from "../Country/country";
+// import "./country.css";
+import "./countries.css";
 
 const Countries = ({ countriesPromise }) => {
-    const countriesData = use(countriesPromise);
-    const countrys = countriesData.countries;
-    // console.log(countriesData); 
+  const countriesData = use(countriesPromise);
+  const countrys = countriesData.countries;
+  // console.log(countriesData);
 
-    
   return (
-    <div>
-      <h1 className="text-2xl font-bold">
-        this is a country display app {countrys.length}</h1>
+    <div className="">
+      <h1 className="country">
+        this is a country display app {countrys.length}
+      </h1>
+      <div className="countries">
         {
-        countrys.map(country => 
-        <Country key={country.cca3.cca3} country={country}></Country>)
+        countrys.map((country) => (<Country key={country.cca3.cca3} country={country}></Country>))
         }
+      </div>
     </div>
   );
 };
